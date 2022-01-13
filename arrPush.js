1d1f4f13b8f2acfa8738cefac4cbd18b781f7edf
Fatimah Emad El-Den <63984422+xxfatj@users.noreply.github.com>
2022-01-13 23:30:50 +0300

    Add files via upload

---
+++ arrPush.js
@@ -0,0 +1,21 @@
+'strict mode'
+
+
+
+function pushArray(pushArr){
+
+    const arr = [];
+
+    for (let i =0; i<array.length; i++){
+        arr.push(pushArr[i]);
+
+        return arr;
+    }
+
+}
+
+const array = [1,2,3,4,5,6,7];
+
+const result = pushArray(array);
+
+console.log(array);
